357c43a7c440f7740c4fb1e9f4e86e217b340632
bkkom <seoy1108@gmail.com>
2023-11-27 14:30:12 +0900

    Feat: 설정 Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Login from "./routes/login";
+import CreateAccount from "./routes/createAccount";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "create-account",
+    element: <CreateAccount />,
+  },
+]);
+
 function App() {
-  return <h1>App</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/layout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <>
+      <h3>Layout</h3>
+      <Outlet />
+    </>
+  );
+}
